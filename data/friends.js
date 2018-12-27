76a9b33ed7bbee71e79e9a4f3c8ecfe32a80891c
LanFP888 <41882036+LanFP888@users.noreply.github.com>
2018-12-27 14:02:46 -0500

    modify links

--- data/friends.js
+++ data/friends.js
@@ -2,6 +2,6 @@ module.exports = [
   {
     name: "Lance",
     connect: "https://www.linkedin.com/in/lance-pu/",
-    scores: ["5", "1", "4", "4", "5", "1", "2", "5", "4", "1"]
+    scores: ["3", "3", "3", "3", "3", "3", "3", "3", "3", "3"]
   }
 ];
