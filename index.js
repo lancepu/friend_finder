9d2b4727ab02ac93e567fc141cffde697ba0c4f7
LanFP888 <41882036+LanFP888@users.noreply.github.com>
2018-12-27 16:22:20 -0500

    add favicon

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const favicon = require("serve-favicon");
 const friends = require("./data/friends");
 const findMatch = require("./calc");
 
@@ -8,6 +9,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(favicon(path.join(__dirname, "favicon.ico")));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "home.html"));
